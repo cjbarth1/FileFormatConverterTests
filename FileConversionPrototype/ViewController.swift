d75d7af00d4ef4220d69ab501d554f39615b2056
Casey Barth <cjbarth1@gmail.com>
2021-08-24 01:31:12 -0400

    Initial Commit

---
+++ FileConversionPrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileConversionPrototype
+//
+//  Created by Casey Barth on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
